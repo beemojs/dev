c0b76bb9bd7464e0bc4f325541d6f52402a83390
Miles Johnson <mileswjohnson@gmail.com>
2021-04-10 17:56:32 -0700

    Add config prettier package.

---
+++ packages/config-prettier/src/index.ts
@@ -0,0 +1,24 @@
+import type { PrettierConfig } from '@beemo/driver-prettier';
+
+const config: PrettierConfig = {
+  ...require('prettier-config-beemo'),
+  ignore: [
+    'node_modules/',
+    // Build folders
+    'build/',
+    'cjs/',
+    'dist/',
+    'esm/',
+    'lib/',
+    'mjs/',
+    // Config files
+    'CHANGELOG.md',
+    'lerna.json',
+    'package.json',
+    'tsconfig.json',
+    'tsconfig.*.json',
+    '*.d.ts',
+  ],
+};
+
+export default config;
