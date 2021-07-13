58a8196ab83a111266d39ae8686bf9c01d8158dd
Miles Johnson <mileswjohnson@gmail.com>
2021-07-12 17:59:34 -0700

    fix: Add missing script name.

--- packages/dev/src/scripts/ReleaseCheck.ts
+++ packages/dev/src/scripts/ReleaseCheck.ts
@@ -1,6 +1,8 @@
 import { Script } from '@beemo/core';
 
 class ReleaseCheck extends Script {
+	override readonly name = 'beemo-script-release-check';
+
 	async execute() {
 		await import('../checks/githubToken');
 	}
